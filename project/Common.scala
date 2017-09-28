e2d07b04f490ea13c7d60293386bf9129422c05a
Bakalov Oleksii <aleo.bakalov@gmail.com>
2017-09-28 14:50:29 +0300

    WIP

--- project/Common.scala
+++ project/Common.scala
@@ -1,7 +1,5 @@
-
-
 object Common {
   def version = "master-SNAPSHOT"
-  def playVersion = System.getProperty("play.version", "2.6.1")
+  def playVersion = System.getProperty("play.version", "2.6.5")
   def scalaVersion = System.getProperty("scala.version", "2.12.3")
 }
